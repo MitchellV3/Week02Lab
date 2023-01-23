d862017a8a4e6654585008fed65c673e447f3bfe
MitchellV3 <mitchellvollmer@shaw.ca>
2023-01-22 20:52:28 -0700

    Add statements that print "JAVA" as ASCII art

--- src/printjavaascii/PrintJavaAscii.java
+++ src/printjavaascii/PrintJavaAscii.java
@@ -14,8 +14,11 @@ public class PrintJavaAscii {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  public static void main(String[] args) {
+    System.out.println("      J    a   v     v  a ");
+    System.out.println("      J   a a   v   v  a a");
+    System.out.println("  J   J  aaaaa   V V  aaaaa");
+    System.out.println("   JJ   a     a   V  a     a");
+  }
     
 }
